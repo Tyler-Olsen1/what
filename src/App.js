fa4e448a584b5fd3df816ae3dbf452f62e0bf43f
Tyler R. Olsen <Tyler@Tylers-MacBook-Air.local>
2019-02-21 10:22:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -22,21 +22,31 @@ async componentWillMount () {
     await this.getSenators()
     }
 
-  render() {
-      console.log(this.state.senators)
-    return (
-      <div>
-        
-           {this.state.senators.map((senator, index) => (
-               <div key={index}>
-                  <h1>Name: {senator.name}, </h1>
-                    Party: {senator.party}
-               </div>
-           ))}
-       
-        </div>
-    );
+filterList= (e) => {
+  let updatedList = this.state.senators
+  updatedList = updatedList.filter((senator) => {
+    return senator.toLowerCase().search(
+      e.target.value.toLowerCase()) !== -1
+  })
+  this.setState({senator: updatedList})
   }
+
+
+render() {
+    console.log(this.state.senators)
+  return (
+    <div>
+      
+          {this.state.senators.map((senator, index) => (
+              <div key={index}>
+                <h1>Name: {senator.name}, </h1>
+                  Party: {senator.party}
+              </div>
+          ))}
+      
+      <Senators poets={this.state.filteredSenators} match={this.props.match} onChange={this.filterSenate} />
+
+      </div>
+  );
 }
-{/* <home /> */}
-export default Home
+
